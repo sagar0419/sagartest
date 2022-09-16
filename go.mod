b5a21af19bcabb07e28f1a96d448c0d908b7c516
Sagar Parmar <sagar.rajput27@gmail.com>
2022-09-16 16:51:31 +0530

    testing

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sagar0419/sagartest
+
+go 1.18
---
+++ if.go
@@ -0,0 +1,11 @@
+package sagartest
+
+import "fmt"
+
+func Neel() {
+	fmt.Println("Hello World")
+}
+
+func sagar() {
+	fmt.Println("Hello World")
+}
